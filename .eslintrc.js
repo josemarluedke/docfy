ebb6f053d08fb5a69abd8229f73456cc4f8fed35
Josemar Luedke <josemarluedke@gmail.com>
2020-04-16 22:07:54 -0700

    Refactor to monorepo and add Linting

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+const { join } = require('path');
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: join(__dirname, './tsconfig.eslint.json')
+  },
+  extends: [
+    '@underline/eslint-config-typescript',
+    '@underline/eslint-config-node'
+  ],
+
+  settings: {
+    node: {
+      tryExtensions: ['.js', '.json', '.node', '.ts']
+    }
+  },
+  rules: {
+    'no-unused-vars': 'off',
+    'node/no-unsupported-features/es-syntax': 'off',
+    'node/no-missing-import': [
+      'error',
+      {
+        allowModules: ['unist'] // this package is only TS types @types/unist
+      }
+    ],
+    '@typescript-eslint/no-empty-interface': 'off'
+  },
+  overrides: [
+    {
+      files: ['packages/**/tests/**/*.ts'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off'
+      }
+    }
+  ]
+};
