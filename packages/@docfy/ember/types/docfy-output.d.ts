8fb1b7562e42676a69137ecb9b416b7075257bc6
Josemar Luedke <josemarluedke@gmail.com>
2020-04-27 20:19:35 -0700

    Use new type of Page

--- packages/@docfy/ember/types/docfy-output.d.ts
+++ packages/@docfy/ember/types/docfy-output.d.ts
@@ -1,8 +1,11 @@
-import { Page } from '@docfy/core/dist/types';
+import { PageContent } from '@docfy/core/dist/types';
 declare module '@docfy/output' {
   // eslint-disable-next-line
   export interface OutputPage
-    extends Pick<Page, 'url' | 'headings' | 'title' | 'source' | 'metadata'> {}
+    extends Pick<
+      PageContent,
+      'url' | 'headings' | 'title' | 'source' | 'metadata'
+    > {}
   const output: OutputPage[];
 
   export default output;
