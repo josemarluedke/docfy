ef2c8655a34ee5d24721922049641d8e890a8c64
Josemar Luedke <josemarluedke@gmail.com>
2020-04-29 20:50:36 -0700

    Implement DocfyPreviousOrNextPage component

---
+++ packages/@docfy/ember/addon/components/docfy-previous-or-next-page.ts
@@ -0,0 +1,68 @@
+import Component from '@glimmer/component';
+import docfyOutput from '@docfy/output';
+import { NestedRuntimeOutput, Page } from '@docfy/core/dist/types';
+import { inject as service } from '@ember/service';
+import RouterService from '@ember/routing/router-service';
+
+interface DocfyPreviousLinkArgs {
+  isPrevious?: boolean;
+  scope?: string;
+}
+
+// We flat the nested output to keep the nested order
+function flatNested(output: NestedRuntimeOutput, pages: Page[] = []): Page[] {
+  pages.push(...output.pages);
+
+  output.children.forEach((child) => {
+    flatNested(child, pages);
+  });
+
+  return pages;
+}
+
+function findPreviousOrNextPage(
+  url: string,
+  isPrevious: boolean,
+  nested: NestedRuntimeOutput
+): Page | undefined {
+  const flat = flatNested(nested);
+  const index = flat.findIndex((page) => {
+    return page.url === url;
+  });
+
+  if (index > -1) {
+    if (isPrevious) {
+      return flat[index - 1];
+    } else {
+      return flat[index + 1];
+    }
+  }
+  return undefined;
+}
+
+export default class DocfyPreviousOrNextLink extends Component<
+  DocfyPreviousLinkArgs
+> {
+  @service router!: RouterService;
+
+  get scopedOutput(): NestedRuntimeOutput {
+    if (typeof this.args.scope === 'string') {
+      const foundScope = docfyOutput.nested.children.find((child) => {
+        return child.name === this.args.scope;
+      });
+
+      if (foundScope) {
+        return foundScope;
+      }
+    }
+    return docfyOutput.nested;
+  }
+
+  get page(): Page | undefined {
+    return findPreviousOrNextPage(
+      this.router.currentURL.split('#')[0],
+      this.args.isPrevious === true,
+      this.scopedOutput
+    );
+  }
+}
