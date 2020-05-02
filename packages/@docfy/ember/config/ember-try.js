c45d2188cde8c9c813ad91a2ff02b14f8bb04064
Josemar Luedke <josemarluedke@gmail.com>
2020-05-01 21:00:24 -0700

    Remove unnecessay ember try scenarios

--- packages/@docfy/ember/config/ember-try.js
+++ packages/@docfy/ember/config/ember-try.js
@@ -38,29 +38,6 @@ module.exports = async function () {
           }
         }
       },
-      // The default `.travis.yml` runs this scenario via `yarn test`,
-      // not via `ember try`. It's still included here so that running
-      // `ember try:each` manually or from a customized CI config will run it
-      // along with all the other scenarios.
-      {
-        name: 'ember-default',
-        npm: {
-          devDependencies: {}
-        }
-      },
-      {
-        name: 'ember-default-with-jquery',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true
-          })
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^0.5.1'
-          }
-        }
-      },
       {
         name: 'ember-classic',
         env: {
