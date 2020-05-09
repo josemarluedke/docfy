8d29321fffc1d5609561825f043e126446fb1997
Josemar Luedke <josemarluedke@gmail.com>
2020-05-09 10:32:33 -0700

    Disable FastBoot when running in Embroider

--- packages/@docfy/ember/ember-cli-build.js
+++ packages/@docfy/ember/ember-cli-build.js
@@ -6,14 +6,34 @@ process.env.EXPERIMENTAL_RENDER_MODE_SERIALIZE = true;
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 const path = require('path');
 
+if (process.env.EMBROIDER) {
+  process.env.FASTBOOT_DISABLED = true;
+}
+
 module.exports = function (defaults) {
+  const trees = {};
+
+  // Exclude FastBoot tests if FASTBOOT_DISABLED is set,
+  // to enable FASTBOOT_DISABLED tests until https://github.com/embroider-build/embroider/issues/160 is resolved.
+  if (process.env.FASTBOOT_DISABLED) {
+    const Funnel = require('broccoli-funnel');
+    trees.tests = new Funnel('tests', {
+      exclude: ['fastboot/**']
+    });
+  }
+
   const app = new EmberAddon(defaults, {
     postcssOptions: {
       compile: {
         enabled: true,
         cacheInclude: [/.*\.css$/, /.tailwind\.config\.js$/],
         plugins: [
-          require('postcss-import'),
+          {
+            module: require('postcss-import'),
+            options: {
+              path: [path.join(__dirname, '../../../node_modules')]
+            }
+          },
           require('tailwindcss')(
             path.join('tests', 'dummy', 'app', 'styles', 'tailwind.config.js')
           ),
@@ -21,6 +41,12 @@ module.exports = function (defaults) {
           require('autoprefixer')
         ]
       }
+    },
+    trees,
+    addons: {
+      blacklist: process.env.FASTBOOT_DISABLED
+        ? ['ember-cli-fastboot-testing']
+        : []
     }
   });
 
