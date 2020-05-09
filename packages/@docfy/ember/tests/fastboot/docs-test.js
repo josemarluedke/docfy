701c3860f067108d190d9483bafb4b2883f298b6
Josemar Luedke <josemarluedke@gmail.com>
2020-05-08 19:52:32 -0700

    Add more FastBoot tests

--- packages/@docfy/ember/tests/fastboot/docs-test.js
+++ packages/@docfy/ember/tests/fastboot/docs-test.js
@@ -18,4 +18,33 @@ module('FastBoot | docs', function (hooks) {
 
     assert.dom('.markdown h1').hasText('Overview');
   });
+
+  test('it renders the docs nav', async function (assert) {
+    await visit('/docs');
+
+    assert
+      .dom('[data-test-id="docs-nav"]')
+      .hasText(
+        'Welcome to Docfy Introduction Installation Overview @docfy/core Overview Helpers genereateFlatOutput genereateNestedOutput @docfy/ember Working with Ember Installation Components Docfy Link Component Docfy Output Component'
+      );
+  });
+
+  test('it renders the previous and next page links', async function (assert) {
+    await visit('/docs/introduction');
+
+    assert
+      .dom('[data-test-id="docs-previous-next"]')
+      .hasText('Previous Welcome to Docfy Next Installation');
+  });
+
+  test('it renders the edit url', async function (assert) {
+    await visit('/docs/introduction');
+
+    assert
+      .dom('[data-test-id="docs-edit-url"]')
+      .hasAttribute(
+        'href',
+        'https://github.com/josemarluedke/docfy/edit/master/packages/@docfy/ember/dummy-docs/introduction.md'
+      );
+  });
 });
