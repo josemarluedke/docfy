837446af30c7a1e8c2d52acce4d93bcac3abb80f
Josemar Luedke <josemarluedke@gmail.com>
2020-05-17 19:46:21 -0700

    Setup @docfy/ember in Site

--- site/app/router.js
+++ site/app/router.js
@@ -1,5 +1,6 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
+import { addDocfyRoutes } from '@docfy/ember';
 
 export default class Router extends EmberRouter {
   location = config.locationType;
@@ -7,5 +8,5 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function () {
-  // Empty
+  addDocfyRoutes(this);
 });
