8114d77a1162ed90755ee5f441396aa1e24deefe
Josemar Luedke <josemarluedke@gmail.com>
2020-05-22 22:00:09 -0700

    Add scroll-to animation for page headings

--- site/app/components/page-headings.ts
+++ site/app/components/page-headings.ts
@@ -1,5 +1,16 @@
 import Component from '@glimmer/component';
+import { scrollToElement } from 'site/utils/scroll-to';
+import { action } from '@ember/object';
 
 interface PageHeadingsArgs {} //eslint-disable-line
 
-export default class PageHeadings extends Component<PageHeadingsArgs> {}
+export default class PageHeadings extends Component<PageHeadingsArgs> {
+  @action onClick(evt: MouseEvent): void {
+    const href = (evt.target as HTMLElement).getAttribute('href');
+    if (href) {
+      const toElement = document.querySelector(href) as HTMLElement;
+
+      scrollToElement(toElement);
+    }
+  }
+}
