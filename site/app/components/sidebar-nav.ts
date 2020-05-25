1e29925ae8747528b63d8d4d3ff7e8c0d6f1027c
Josemar Luedke <josemarluedke@gmail.com>
2020-05-24 21:11:39 -0700

    Add the simplest mobile menu for content

--- site/app/components/sidebar-nav.ts
+++ site/app/components/sidebar-nav.ts
@@ -1,5 +1,13 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 interface SidebarNavArgs {} //eslint-disable-line
 
-export default class SidebarNav extends Component<SidebarNavArgs> {}
+export default class SidebarNav extends Component<SidebarNavArgs> {
+  @tracked isOpen = false;
+
+  @action toggle(): void {
+    this.isOpen = !this.isOpen;
+  }
+}
