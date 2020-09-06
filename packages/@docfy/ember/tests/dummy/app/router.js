79f7b7eadde25d030089d99cebd88bdc29fdba3f
Josemar Luedke <josemarluedke@gmail.com>
2020-09-05 19:18:44 -0700

    Upgrade @docfy/ember to Ember v3.21

--- packages/@docfy/ember/tests/dummy/app/router.js
+++ packages/@docfy/ember/tests/dummy/app/router.js
@@ -1,5 +1,5 @@
 import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import config from 'dummy/config/environment';
 import { addDocfyRoutes } from '@docfy/ember';
 
 export default class Router extends EmberRouter {
