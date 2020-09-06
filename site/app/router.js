5afb9680641b3d462ecb9490472533e09d43f22c
Josemar Luedke <josemarluedke@gmail.com>
2020-09-05 19:23:15 -0700

    Upgrade site to Ember v3.21

--- site/app/router.js
+++ site/app/router.js
@@ -1,5 +1,5 @@
 import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import config from 'site/config/environment';
 import { addDocfyRoutes } from '@docfy/ember';
 
 export default class Router extends EmberRouter {
