f35aade9ddf1c94c607ce7aa29be8e89ee2d3307
Josemar Luedke <josemarluedke@gmail.com>
2021-01-26 21:22:46 -0800

    WIP

--- packages/core/types/trough.d.ts
+++ packages/core/types/trough.d.ts
@@ -1,6 +1,6 @@
 declare module 'trough' {
   class Through<T = unknown> {
-    use<T1 = T>(fn: (data: T1) => void | T, bla?: unknown): this;
+    use<T1 = T>(fn: (data: T1) => void | T): this;
 
     run(input: unknown, done: (err: unknown, data: T) => void): T;
   }
